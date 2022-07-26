a0f027da081241af764647f847dce640900e94d0
marchewazz <mateuszooospot@gmail.com>
2022-07-26 23:11:48 +0200

    Calendar

--- frontend/src/app/components/logged-state/calendar-dashboard/calendar-dashboard.component.ts
+++ frontend/src/app/components/logged-state/calendar-dashboard/calendar-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { monthsNames, daysNames } from 'src/app/utilities';
 
 @Component({
   selector: 'app-calendar-dashboard',
@@ -9,9 +12,27 @@ export class CalendarDashboardComponent implements OnInit {
 
   @Input() userData: any = {};
   
-  constructor() { }
+  daysControl: FormControl = new FormControl(new Date().getDate().toString());
+  monthsControl: FormControl = new FormControl(new Date().getMonth().toString());
+  yearsControl: FormControl = new FormControl(new Date().getFullYear().toString());
 
-  ngOnInit(): void {
+  monthsNames: string[] = monthsNames;
+  daysNames: string[] = daysNames;
+  years: string[] = [];
+  days: Date[] = [];
+
+  constructor() {
+    for(let i = 2010; i<= 2024; i++) this.years.push(i.toString())
   }
 
+  ngOnInit(): void {
+    this.getNumberOfDays()
+  }
+
+  getNumberOfDays(): void {
+    this.days = [];
+    const numberOfDays = new Date(this.yearsControl.value, this.monthsControl.value, 0).getDate();
+    for(let i = 1; i <= numberOfDays; i++) this.days.push(new Date(this.yearsControl.value, this.monthsControl.value - 1, i))
+    if (parseInt(this.daysControl.value) > numberOfDays) this.daysControl.setValue(numberOfDays.toString());
+  }
 }
---
+++ frontend/src/app/utilities.ts
@@ -0,0 +1,3 @@
+export const monthsNames: string[] = [ "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December" ];
+export const daysNames: string[] = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
