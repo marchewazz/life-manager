db5f55a739c3831f5770da55ba43232d3450c0bf
marchewazz <mateuszooospot@gmail.com>
2022-08-15 01:03:02 +0200

    Styling

--- backend/src/controllers/MoneyController.ts
+++ backend/src/controllers/MoneyController.ts
@@ -6,7 +6,7 @@ import createRandomString from "../utilities/CreateRandomString";
 class MoneyController {
     public async updateBalance(data: any) {
         const collection = MongoDBClient.db("life-manager").collection("accounts");
-        collection.updateOne({"_id": new ObjectId(data.userID)}, {
+        await collection.updateOne({"_id": new ObjectId(data.userID)}, {
             $set: {
                 balance: parseFloat(data.newBalance)
         }})
